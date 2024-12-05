abb6f60974b36ec0c9cf5334e5d6efe40e5181cc
Batug Kocak <batugkocak@gmail.com>
2024-12-05 19:32:31 +0300

    add entity, DTO and service to episodes module

---
+++ src/episodes/entity/episode.entity.ts
@@ -0,0 +1,5 @@
+export default class Episode {
+  id: string;
+  name: string;
+  featured: boolean;
+}
---
+++ src/episodes/episodes.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import Episode from './entity/episode.entity';
+import { randomUUID } from 'crypto';
+import { CreateEpisodeDTO } from './dto/episode.dto';
+
+@Injectable()
+export class EpisodesService {
+  private episodes: Episode[] = [];
+
+  async findAll(sort: 'asc' | 'desc' = 'asc'): Promise<Episode[]> {
+    const sortAsc = (a: Episode, b: Episode) => (a.name > b.name ? 1 : -1);
+    const sortDesc = (a: Episode, b: Episode) => (a.name < b.name ? 1 : -1);
+
+    return sort === 'asc'
+      ? this.episodes.sort(sortAsc)
+      : this.episodes.sort(sortDesc);
+  }
+
+  async findFeatured(): Promise<Episode> {
+    return this.episodes.find((episode) => episode.featured);
+  }
+  async findOne(id: string): Promise<Episode> {
+    return this.episodes.find((episode) => episode.id === id);
+  }
+  async create(createEpisodeDto: CreateEpisodeDTO) {
+    const newEpisode = { ...createEpisodeDto, id: randomUUID() };
+    this.episodes.push(newEpisode);
+    return newEpisode;
+  }
+}
