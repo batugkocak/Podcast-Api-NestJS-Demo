a7cc2b5e886e7f6b030bfd11ed19f2a2cb956a9f
Batug Kocak <batugkocak@gmail.com>
2024-12-06 11:04:36 +0300

    add EpisodesService and ConfigModule to EpisodesController tests for doing tests

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { EpisodesService } from './episodes.service';
+import { ConfigModule } from '../config/config.module';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
-
+  // Before writing each test, we have to provide the Service and import the ConfigModule as we are doing it in the controller itself.
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EpisodesController],
+      providers: [EpisodesService],
+      imports: [ConfigModule],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
