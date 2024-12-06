cc02eaa3050a618a7780bce3a464ee247054c713
Batug Kocak <batugkocak@gmail.com>
2024-12-06 11:13:06 +0300

    add mock service to EpisodesController tests and implement findOne test case

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -5,11 +5,19 @@ import { ConfigModule } from '../config/config.module';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
+
+  const mockEpisodesService = {
+    findAll: async () => [{ id: 'id' }],
+    findFeaturedEpisodes: async () => [{ id: 'id' }],
+    findOne: async () => ({ id: 'id' }),
+    create: async () => ({ id: 'id' }),
+  };
+
   // Before writing each test, we have to provide the Service and import the ConfigModule as we are doing it in the controller itself.
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EpisodesController],
-      providers: [EpisodesService],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService }],
       imports: [ConfigModule],
     }).compile();
 
@@ -18,5 +26,13 @@ describe('EpisodesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return correct response', async () => {
+      const episodeID = 'id';
+      const result = await controller.findOne(episodeID);
+      expect(result).toEqual({ id: 'id' });
+    });
   });
 });
